c292ddb4a49498747a914b33bef669cc515fcc16
stevensassoc <it@stevens-assoc.com>
2026-06-13 17:56:52 -0400

    Publish test-3

---
+++ test-3/tour.js
@@ -0,0 +1,52 @@
+window.TOUR = {
+  "meta": {
+    "title": "Test 3",
+    "subtitle": "2026-06-13",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true
+  },
+  "scenes": [
+    {
+      "id": "r0010030",
+      "name": "R0010030",
+      "image": "assets/panos/r0010030.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8591498035317864,
+          "pitch": -0.27620968928374157,
+          "rotation": 4.71238898038469,
+          "target": "r0010029",
+          "label": ""
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "r0010029",
+      "name": "R0010029",
+      "image": "assets/panos/r0010029.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3722248954868863,
+          "pitch": -0.1691121115731354,
+          "rotation": 4.71238898038469,
+          "target": "r0010030",
+          "label": ""
+        }
+      ],
+      "infoHotspots": []
+    }
+  ]
+};
