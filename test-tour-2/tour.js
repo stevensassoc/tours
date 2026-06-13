c45e5e629dd5044919dec4ed81e898eacd70495c
stevensassoc <it@stevens-assoc.com>
2026-06-13 16:03:51 -0400

    Publish test-tour-2

---
+++ test-tour-2/tour.js
@@ -0,0 +1,66 @@
+window.TOUR = {
+  "meta": {
+    "title": "Test Tour 2",
+    "subtitle": "",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true
+  },
+  "scenes": [
+    {
+      "id": "r0010030",
+      "name": "R0010030",
+      "image": "assets/panos/r0010030.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8459829569854145,
+          "pitch": -0.22647288645732738,
+          "rotation": 4.71238898038469,
+          "target": "r0010029",
+          "label": ""
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3124184982394045,
+          "pitch": -0.025620547032198723,
+          "title": "TV",
+          "text": "Yes, it's crooked."
+        }
+      ]
+    },
+    {
+      "id": "r0010029",
+      "name": "R0010029",
+      "image": "assets/panos/r0010029.jpg",
+      "initialView": {
+        "yaw": -1.4061520760612765,
+        "pitch": -0.3963426988398344,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3901548706518438,
+          "pitch": -0.15482946688944235,
+          "rotation": 4.71238898038469,
+          "target": "r0010030",
+          "label": "n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1749558853672581,
+          "pitch": -0.01905759536273387,
+          "title": "Cows",
+          "text": "Who doesn't like cows?"
+        }
+      ]
+    }
+  ]
+};
