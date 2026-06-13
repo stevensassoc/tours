2b2d8f47ea08f7db95e96af214b5c9e54b019d2c
stevensassoc <it@stevens-assoc.com>
2026-06-13 15:32:35 -0400

    Publish untitled-tour

---
+++ untitled-tour/tour.js
@@ -0,0 +1,59 @@
+window.TOUR = {
+  "meta": {
+    "title": "Untitled Tour",
+    "subtitle": "",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true
+  },
+  "scenes": [
+    {
+      "id": "r0010029",
+      "name": "R0010029",
+      "image": "assets/panos/r0010029.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4256391070303192,
+          "pitch": -0.15678969865062875,
+          "rotation": 4.71238898038469,
+          "target": "r0010030",
+          "label": ""
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "r0010030",
+      "name": "R0010030",
+      "image": "assets/panos/r0010030.jpg",
+      "initialView": {
+        "yaw": -1.5145260397679206,
+        "pitch": -0.512049667773382,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8588634479991692,
+          "pitch": -0.22577376053530096,
+          "rotation": 4.71238898038469,
+          "target": "r0010029",
+          "label": ""
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2456866791488208,
+          "pitch": -0.05098871307522365,
+          "title": "Test Note",
+          "text": "TV is crooked"
+        }
+      ]
+    }
+  ]
+};
