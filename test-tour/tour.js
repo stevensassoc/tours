16015f3cfa85cc2a9145a36fe9457e175ba914a4
stevensassoc <it@stevens-assoc.com>
2026-06-14 17:25:58 -0400

    Publish test-tour

---
+++ test-tour/tour.js
@@ -0,0 +1,36 @@
+window.TOUR = {
+  "meta": {
+    "title": "Test Tour ",
+    "subtitle": "",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true
+  },
+  "scenes": [
+    {
+      "id": "r0010030",
+      "name": "R0010030",
+      "image": "assets/panos/r0010030.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "r0010029",
+      "name": "R0010029",
+      "image": "assets/panos/r0010029.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ]
+};
