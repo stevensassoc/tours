904f36ff307bb575a76cb7d39c31c3d5d94573f4
stevensassoc <it@stevens-assoc.com>
2026-06-19 10:39:48 -0400

    Publish untitled-tour

---
+++ untitled-tour/tour.js
@@ -0,0 +1,85 @@
+window.TOUR = {
+  "meta": {
+    "title": "Untitled Tour",
+    "subtitle": "",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true,
+    "cover": "assets/cover.jpg"
+  },
+  "scenes": [
+    {
+      "id": "r0010029",
+      "name": "R0010029",
+      "image": "assets/panos/r0010029.jpg",
+      "initialView": {
+        "yaw": -1.3662805543329615,
+        "pitch": -0.20647288652494317,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3396735317653186,
+          "pitch": -0.14849009489709336,
+          "rotation": 4.71238898038469,
+          "target": "r0010030",
+          "label": ""
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1061871149235647,
+          "pitch": -0.193818701218787,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -0.7821148966714017,
+          "pitch": -0.26046736713255114,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -1.120991030804217,
+          "pitch": -0.20121895126274955,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -1.251007355690544,
+          "pitch": -0.039308612236575974,
+          "title": "Note",
+          "text": "Another test"
+        }
+      ]
+    },
+    {
+      "id": "r0010030",
+      "name": "R0010030",
+      "image": "assets/panos/r0010030.jpg",
+      "initialView": {
+        "yaw": -1.4102457901539094,
+        "pitch": -0.19614909649942494,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8810970844689248,
+          "pitch": -0.27619590719364334,
+          "rotation": 4.71238898038469,
+          "target": "r0010029",
+          "label": ""
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3871847649233924,
+          "pitch": -0.017696698854273312,
+          "title": "Note",
+          "text": "test note"
+        }
+      ]
+    }
+  ]
+};
