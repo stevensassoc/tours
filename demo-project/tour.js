494f68b8b43dfaf29fe63b3f5069de7878e49c9b
stevensassoc <it@stevens-assoc.com>
2026-06-24 15:23:55 -0400

    Publish demo-project

---
+++ demo-project/tour.js
@@ -0,0 +1,116 @@
+window.TOUR = {
+  "meta": {
+    "title": "Demo Project",
+    "subtitle": "Stevens - 062426",
+    "logo": "assets/logo.png",
+    "accent": "#3a8f9c",
+    "autoRotate": false,
+    "showWelcome": true
+  },
+  "scenes": [
+    {
+      "id": "r0010031",
+      "name": "R0010031",
+      "image": "assets/panos/r0010031.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8855146569287529,
+          "pitch": -0.17298967603616064,
+          "rotation": 3.839724354387525,
+          "target": "r0010032",
+          "label": ""
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "r0010032",
+      "name": "R0010032",
+      "image": "assets/panos/r0010032.jpg",
+      "initialView": {
+        "yaw": 1.6701704185473276,
+        "pitch": -0.11739800807806954,
+        "fov": 1.7453292519943295
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.803823855939469,
+          "pitch": -0.23881838309789236,
+          "rotation": 4.537856055185257,
+          "target": "r0010031",
+          "label": "sample"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4939020620582495,
+          "pitch": -0.07852828424462464,
+          "title": "Test",
+          "text": "Note these fancy degrees"
+        }
+      ]
+    },
+    {
+      "id": "r0010033",
+      "name": "R0010033",
+      "image": "assets/panos/r0010033.jpg",
+      "initialView": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7270431963409054,
+          "pitch": -0.1356619186023522,
+          "rotation": 4.71238898038469,
+          "target": "r0010031",
+          "label": ""
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1895782307223082,
+          "pitch": 0.03006130448553461,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -0.6304411578450311,
+          "pitch": 0.037236243772973666,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -1.5635600677586836,
+          "pitch": -0.09704144440813098,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -2.4512596271609137,
+          "pitch": 0.03884481871320269,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": -3.1045429539556224,
+          "pitch": -0.04063306097993644,
+          "title": "Note",
+          "text": ""
+        },
+        {
+          "yaw": 2.436547045987509,
+          "pitch": -0.0874228652166296,
+          "title": "Note",
+          "text": ""
+        }
+      ]
+    }
+  ]
+};
